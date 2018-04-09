a1e5a59c1908773a203a2e8d6e1ee2b1e86afbd7
Me <dstark85@gmail.com>
2018-04-08 18:47:50 -0700

    upload mid-week notes

--- r-programming/week2/notes.R
+++ r-programming/week2/notes.R
@@ -4,3 +4,48 @@
 # Control structures
 # Functions
 # Scoping
+
+# Control structures are similar to C
+if(0 == 0) {
+  x <- 0
+} else {
+  x <- 1
+}
+
+# They can be used in a slighly different way though
+x <- if(1) {100} else {
+  0
+}
+
+# For loops, on the other hand, are quite similar to Python.
+for(i in 1:10) {
+  print(i)
+}
+
+# Seq_along is about the eqivalent to range(len(OBJECT))
+my_name <- c("D", "e", "r", "e", "k")
+for(i in seq_along(my_name)) {
+  print(my_name[i])
+}
+
+# Python once again:
+for(letter in my_name) {
+  print(letter)
+}
+
+# While is practically C.
+count <- 0
+while(count < 10) {
+  count <- count + 1
+  print(count)
+}
+
+# Subset Function
+subset_fun <-  function(v) {
+  v[v > 10]
+}
+
+# Mean of each column in a data frame
+df_mean <- function(df) {
+ for(column in seq_len(ncol(df))) {x[column] <- (mean(df[,column]))} 
+}
